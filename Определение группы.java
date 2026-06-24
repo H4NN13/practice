fa2efba71e5bba073b29d9d8af1ae192ece567c2
H4NN13 <65667515+H4NN13@users.noreply.github.com>
2026-06-24 16:51:07 +0300

    Create Определение группы.java

---
+++ Определение группы.java
@@ -0,0 +1,18 @@
+public class task10 {
+    public static void main(String[] args){
+        int result = determineGroup(8);
+        System.out.println(result);
+
+    }
+    public static int determineGroup(int age){
+        if (age >= 7 && age <= 13){
+            return 1;
+        } else if (age >= 14 && age <=17){
+            return 2;
+        }else if (age >= 18 && age <=65){
+            return 3;
+        }else{
+            return -1;
+        }
+    }
+}
